1b5e9c565a4c26ea74357750251caa360ee4a538
NicholasTheisen <nicholas.theisen1997@gmail.com>
2023-12-22 07:16:07 +0000

    ISS Spotter V1.5

---
+++ index2.js
@@ -0,0 +1,20 @@
+// index2.js
+
+const { fetchMyIP } = require('./iss_promised');
+
+fetchMyIP()
+  .then(fetchCoordsByIP)
+  .then(fetchISSFlyOverTimes)
+  .then(body => console.log(body));
+
+const { nextISSTimesForMyLocation } = require('./iss_promised');
+
+nextISSTimesForMyLocation()
+  .then((passTimes) => {
+    printPassTimes(passTimes);
+  })
+  .catch((error) => {
+    console.log("It didn't work: ", error.message);
+  });
+
+  
